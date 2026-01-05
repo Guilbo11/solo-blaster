da076e5a5fd81003b622c7685dcb93f2614ba3fe
Guilbo11 <168447089+Guilbo11@users.noreply.github.com>
2026-01-05 16:04:40 -0500

    Delete solo-blaster-v1 directory

--- solo-blaster-v1/src/utils/uuid.ts
+++
@@ -1,8 +0,0 @@
-export function uuid(): string {
-  // RFC4122-ish, good enough for local app state
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-    const r = (Math.random() * 16) | 0;
-    const v = c === 'x' ? r : (r & 0x3) | 0x8;
-    return v.toString(16);
-  });
-}
